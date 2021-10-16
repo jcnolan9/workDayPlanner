0ab376b7b5aec6fcd4002eb1bdece75d01556a76
Mbaku Starcrush <j.c.nolan9@gmail.com>
2021-10-16 13:09:37 -0700

    Fixing logic to add colors to the textareas based on time of day

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,37 @@ var activities = []
 var textAreasArray = container.children().children().children("textarea")
 // console.log(textAreasArray)
 
+// console.log("Moment == moment: " + moment().isSame(moment()))
+
 for(var i = 9; i < workTimes.length + 9; i++) {
-    hour.hour(i).format("hA")
-    workTimes[i-9].textContent = hour.format("hA")
-    if(hour < moment()) {
-        colorArea.addClass("past")
+    hour.hour(i).minute(0).second(0)
+    console.log("Start of hour: " + hour.hour(i).minute(0).second(0).format('MMMM Do YYYY, h:mm:ss a'))
+    console.log("End of hour: " + hour.hour(i).minute(59).second(59).format('MMMM Do YYYY, h:mm:ss a'))
+    console.log("Current time: " + moment().format('MMMM Do YYYY, h:mm:ss a'))
+    console.log("Is current time after end of hour: " + moment().isAfter(hour.minute(59).second(59)))
+    workTimes[i-9].textContent = hour.format("hA") 
+    
+    if(moment().isAfter(hour.minute(59).second(59))) {
+        // console.log(hour.minute(59).second(59))
+        // console.log("Hour (past): " + hour.format('MMMM Do YYYY, h:mm:ss a'))
+        // console.log("Current moment (past): " + moment().format('MMMM Do YYYY, h:mm:ss a'))
+        console.log(colorArea[i-9])
+        $(colorArea[i-9]).addClass("past")
+        console.log("PAST +++++++++++++++++++++++++++++++++++++")
     }
-    else if(hour == moment()) {
-        colorArea.addClass("present")
+    else if(moment().isSame(hour, "hour")) {
+        // console.log("Hour (present): " + hour.format('MMMM Do YYYY, h:mm:ss a'))
+        // console.log("Current moment (present): " + moment().format('MMMM Do YYYY, h:mm:ss a'))
+        // colorArea[i].addClass("present")
+        $(colorArea[i-9]).addClass("present")
+        console.log("Present -------------------------------------")
     }
     else {
-        colorArea.addClass("future")
+        // console.log("Hour (future): " + hour.format('MMMM Do YYYY, h:mm:ss a'))
+        // console.log("Current moment (future): " + moment().format('MMMM Do YYYY, h:mm:ss a'))
+        // colorArea[i].addClass("future")
+        $(colorArea[i-9]).addClass("future")
+        console.log("Future ========================================")
     }
 }
 
